e4d72709a796c0c8f6e60f30cb7e45f7b865e77e
Michael Scott <jms@colorado.edu>
2016-04-23 23:28:30 -0600

    Made some changes to the struct and rearranged some methods to reflect the source file.

--- Final_Project.h
+++ Final_Project.h
@@ -10,36 +10,57 @@ struct DecisionNode
   DecisionTree *leftChild;
   DecisionTree *rightChild;
   vector <std::string> solutions;
-  int counter; //Counts the number of times the node is utilized.
+  //int leftCounter; //Counts the number of times the left node is selected
+  //int rightCounter; //Counts the number of times the right node is selected
+  int solCount; //Counts the number of solutions stored in this node
+  std::string key;
 
-  DecisionTree(){};
+  DecisionNode()
+  {
+    parent = NULL;
+    leftChild = NULL;
+    rightChild = NULL;
+    solCount = 0;
+  };
 
-  DecisionTree(string in_cat)
+  DecisionNode(std::string in_cat, std::string in_key)
   {
     category = in_cat;
     parent = NULL;
     leftChild = NULL;
     rightChild = NULL;
-  }
+    //leftCounter = 0; TODO determine if these are necessary
+    //rightCounter = 0;
+    key = in_key;
+    solCount = 0;
+  };
 }
 
 class DecisionTree
 {
+  //TODO Define parameters for these functions. DeleteTree function?
   public:
     DecisionTree();
     ~DecisionTree();
-    void balanceTree();
-    void addDecisionNode();
-    void addSolution();
-    void shortestPath();
+    void addDecisionNode(std::string, DecisionNode, std::string);
+    void buildDecisionTree(std::string, std:: string);
+    void bulkSolutionAdd(std::string, std::string);
     void countSolutions(); //Recursive loop that provides a total count of solutions
-    void printSolutions();
+    void deleteTree();
     void printQuestions();
+    void run20Questions();
+    void printSolutions();
+    void shortestPath(std::string);
+    //void balanceTree(); REVIEW can probably remove this. Will most likely be too technically challenging.
   protected:
   private:
     DecisionNode *root;
-    DecisionNode searchTree();
-    void leftRottate();
-    void rightRotate();
+    DecisionNode searchSolutions(DecisionNode, std::string);
+    DecisionNode searchTree(DecisionNode, std::string);
+    void addSolution(std::string, DecisionNode);
+    void leftRottate(); //REVIEW can probably remove
+    void rightRotate(); //REVIEW can probably remove
     void maxHeightCheck(); //Makes sure that we don't ask more than 20 questions
 }
+
+#endif //FINAL_PROJECT_H
